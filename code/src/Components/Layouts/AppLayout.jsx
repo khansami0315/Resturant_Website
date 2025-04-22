0b89ef7abae8d408a8ea56566bf140a0f37d268f
Abdul Sami <168129414+khansami0315@users.noreply.github.com>
2025-04-23 01:11:40 +0500

    Update AppLayout.jsx

--- code/src/Components/Layouts/AppLayout.jsx
+++ code/src/Components/Layouts/AppLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 import { Footer } from "../../Ul/Footer"
 import { Header } from "../../Ul/Header"
 
-export const AppLayout  = () => {
+ export default  AppLayout  = () => {
 
 
 
